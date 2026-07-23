e3934e4abf7027cdb91e990dafc1c1b8c68b4715
kartikeyofficial <kartikeygithubproject@gmail.com>
2026-07-24 01:04:42 +0530

    Quick Sorting

--- DivideAndConquer/src/quickSorting.java
+++ DivideAndConquer/src/quickSorting.java
@@ -1,43 +1,43 @@
 public class quickSorting {
     static void main(String[] args) {
-        int arr[] = {6,3,8,9,1,2,7};
-        quickSort(arr,0, arr.length-1);
+        int[] arr = {6,3,2,7,5,9,1};
+        QuickSort(arr,0,arr.length-1);
         printArr(arr);
 
     }
+
     public static void printArr(int[] arr){
         for (int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
+            System.out.print(arr[i]+",");
         }
         System.out.println();
     }
-    public static void quickSort(int[] arr, int si, int ei){
-        if (si >= ei){
+    public static void QuickSort(int[] arr,int si, int ei){
+        if (si>=ei){
             return;
         }
-        // pivot is last element
-        int pidx = partition(arr,si,ei);
-        quickSort(arr, si, pidx-1); // left
-        quickSort(arr, pidx+1, ei); // right
+        int pindx =partition(arr,si,ei);
+        QuickSort(arr,si,pindx-1);  // left part
+        QuickSort(arr,pindx+1,ei);  // Right Part
     }
     public static int partition(int[] arr, int si, int ei){
-        int pivot = arr[ei];
-        int i = si-1; // to make place for smaller than pivot
-
-        for (int j=si; j<ei; j++){
-            if (arr[j]<= pivot){
-                i++;
-                // swap
-                int temp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = temp;
-            }
-        }
+       int pivot = arr[ei];
+       int i = si-1;
+       for (int j=si;j<ei;j++){
+           if (arr[j]<=pivot){
+               i++;
+               // Swap
+               int temp = arr[j];
+               arr[j]= arr[i];
+               arr[i] = temp;
+           }
+       }
         i++;
+        // Swap
         int temp = pivot;
-        arr[ei] = arr[i]; // pivot = arr[i]  not be write here
+        arr[ei] = arr[i];  // pivot = arr[i] don't be written.
         arr[i] = temp;
-        return  i;
+        return i;
 
     }
 }
