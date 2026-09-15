fbf3718431247c4ed96bd69f2e70a76076453762
kartikeyofficial <kartikeygithubproject@gmail.com>
2026-09-15 12:28:31 +0530

    Arraylist Methods

---
+++ ArrayList/src/SortingList.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortingList {
+    static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(1);
+        list.add(6);
+        list.add(2);
+        list.add(4);
+        list.add(7);
+
+        Collections.sort(list);
+        System.out.println("Ascending Order: "+list);
+        Collections.sort(list,Collections.reverseOrder());
+        System.out.println("Descending Order: "+list);
+
+    }
+
+
+
+}
